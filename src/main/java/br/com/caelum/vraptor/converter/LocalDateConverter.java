faa6b37f778fc5f6b9980ca9acf61d0d583d0987
Otávio Garcia <otavio@otavio.com.br>
2014-07-07 23:09:53 -0300

    Fixing indentation

--- src/main/java/br/com/caelum/vraptor/converter/LocalDateConverter.java
+++ src/main/java/br/com/caelum/vraptor/converter/LocalDateConverter.java
@@ -1,19 +1,19 @@
 /***
-* Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
-* All rights reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package br.com.caelum.vraptor.converter;
 
@@ -31,43 +31,44 @@ import javax.inject.Inject;
 import br.com.caelum.vraptor.Convert;
 
 /**
-* A converter for {@link LocalDate}.
-*
-* @author Lucas Cavalcanti
-* @author Otávio Scherer Garcia
-*/
+ * A converter for {@link LocalDate}.
+ *
+ * @author Lucas Cavalcanti
+ * @author Otávio Scherer Garcia
+ */
 @Convert(LocalDate.class)
 @RequestScoped
-public class LocalDateConverter implements Converter<LocalDate> {
+public class LocalDateConverter
+	implements Converter<LocalDate> {
 
-private final Locale locale;
+	private final Locale locale;
 
-/**
-* @deprecated CDI eyes only
-*/
-protected LocalDateConverter() {
-this(null);
-}
+	/**
+	 * @deprecated CDI eyes only
+	 */
+	protected LocalDateConverter() {
+		this(null);
+	}
 
-@Inject
-public LocalDateConverter(Locale locale) {
-this.locale = locale;
-}
+	@Inject
+	public LocalDateConverter(Locale locale) {
+		this.locale = locale;
+	}
 
-@Override
-public LocalDate convert(String value, Class<? extends LocalDate> type) {
-if (isNullOrEmpty(value)) {
-return null;
-}
+	@Override
+	public LocalDate convert(String value, Class<? extends LocalDate> type) {
+		if (isNullOrEmpty(value)) {
+			return null;
+		}
 
-try {
-return LocalDate.parse(value, getFormatter());
-} catch (DateTimeParseException e) {
-throw new ConversionException(new ConversionMessage("is_not_a_valid_date", value));
-}
-}
+		try {
+			return LocalDate.parse(value, getFormatter());
+		} catch (DateTimeParseException e) {
+			throw new ConversionException(new ConversionMessage("is_not_a_valid_date", value));
+		}
+	}
 
-protected DateTimeFormatter getFormatter() {
-return DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).withLocale(locale);
-}
+	protected DateTimeFormatter getFormatter() {
+		return DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).withLocale(locale);
+	}
 }
